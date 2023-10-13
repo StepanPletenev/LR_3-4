c54cc6da4e36bd35e6cfef48cdefd3e298a3faff
StepanPletenev <144107101+StepanPletenev@users.noreply.github.com>
2023-10-13 15:19:32 +0300

    Add files via upload

---
+++ lr3_4_16.pas
@@ -0,0 +1,24 @@
+﻿program lr3_4_16;
+var n,i:integer;
+begin
+repeat
+writeln('Введите натуральное число n=');
+readln(n);
+until n>0;
+if n=1 then write('Простых множителей нет')
+else
+ begin
+  write(n,'=');
+  i:=2;
+  while i<=n do
+   begin
+    while n mod i=0 do
+     begin
+      if i<n then write(i,'*')
+      else writeln(i);
+      n:=n div i;
+     end;
+    i:=i+1;
+   end;
+  end;
+end.
